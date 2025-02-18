71909d308c9136ac141a7b3c30a4399821494028
TEESDAAL <alanteesdale@proton.me>
2025-02-18 22:06:10 +1300

    added examples

---
+++ examples/symbolic_regression.py
@@ -0,0 +1,93 @@
+import sys
+# hack to import from parent directory
+sys.path.append("../")
+
+from deap_tree import Tree, TreeDrawer
+# This code is grabbed from the deap examples page (https://github.com/DEAP/deap/blob/0ebb47e34298885840fa7474fa9ffb9c7d7f2c8d/examples/gp/symbreg.py)
+
+import operator
+import math
+import random
+
+import numpy
+
+from functools import partial
+
+from deap import algorithms
+from deap import base
+from deap import creator
+from deap import tools
+from deap import gp
+
+# Define new functions
+def protectedDiv(left, right):
+    try:
+        return left / right
+    except ZeroDivisionError:
+        return 1
+
+pset = gp.PrimitiveSet("MAIN", 1)
+pset.addPrimitive(operator.add, 2)
+pset.addPrimitive(operator.sub, 2)
+pset.addPrimitive(operator.mul, 2)
+pset.addPrimitive(protectedDiv, 2)
+pset.addPrimitive(operator.neg, 1)
+pset.addPrimitive(math.cos, 1)
+pset.addPrimitive(math.sin, 1)
+pset.addEphemeralConstant("rand101", partial(random.randint, -1, 1))
+pset.renameArguments(ARG0='x')
+
+creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
+
+toolbox = base.Toolbox()
+toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=1, max_=2)
+toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+toolbox.register("compile", gp.compile, pset=pset)
+
+def evalSymbReg(individual, points):
+    # Transform the tree expression in a callable function
+    func = toolbox.compile(expr=individual)
+    # Evaluate the mean squared error between the expression
+    # Function changed for this example
+    sqerrors = ((func(x) - (math.sin(x**2) - x))**2 for x in points)
+    return math.fsum(sqerrors) / len(points),
+
+toolbox.register("evaluate", evalSymbReg, points=[x/10. for x in range(-10,10)])
+toolbox.register("select", tools.selTournament, tournsize=3)
+toolbox.register("mate", gp.cxOnePoint)
+toolbox.register("expr_mut", gp.genFull, min_=0, max_=2)
+toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
+
+toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value=10))
+toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value=10))
+
+def main():
+    random.seed(318)
+
+    pop = toolbox.population(n=300)
+    hof = tools.HallOfFame(1)
+
+    stats_fit = tools.Statistics(lambda ind: ind.fitness.values)
+    stats_size = tools.Statistics(len)
+    mstats = tools.MultiStatistics(fitness=stats_fit, size=stats_size)
+    mstats.register("avg", numpy.mean)
+    mstats.register("std", numpy.std)
+    mstats.register("min", numpy.min)
+    mstats.register("max", numpy.max)
+
+    pop, log = algorithms.eaSimple(pop, toolbox, 0.5, 0.1, 40, stats=mstats,
+                                   halloffame=hof, verbose=True)
+    # print log
+    return pop, log, hof
+
+# My code now
+if __name__ == "__main__":
+    _, _, hof = main()
+    best_individual = hof[0]
+    tree = Tree.of(best_individual, pset)
+    # Visualise for some specific input
+    TreeDrawer().save_graph("base_tree_drawer_example.png", tree, 7)
+
+
